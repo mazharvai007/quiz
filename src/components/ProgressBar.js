2a1b530d7ec376d79e6451551570f76f9dd42231
Mazharul Islam <swopnil420@gmail.com>
2024-04-16 11:02:58 +0600

    Added answers, progressbar and miniplayer components

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,31 @@
+import classes from '../styles/ProgressBar.module.css';
+function ProgressBar() {
+	return (
+		<>
+			<div className={classes.progressBar}>
+				<div className={classes.backButton}>
+					<span className="material-icons-outlined">arrow_back</span>
+				</div>
+				<div className={classes.rangeArea}>
+					<div className={classes.tooltip}>24% Cimplete!</div>
+					<div className={classes.rangeBody}>
+						<div
+							className={classes.progress}
+							// style="width: 20%"
+						></div>
+					</div>
+				</div>
+				<a href="result.html">
+					<button className={`${classes.button} next`}>
+						<span>Next Question</span>
+						<span className="material-icons-outlined">
+							arrow_forward
+						</span>
+					</button>
+				</a>
+			</div>
+		</>
+	);
+}
+
+export default ProgressBar;
