bf52a79aafca0ac5df8b0053f57546ec7da70173
mazhar <swopnil420@gmail.com>
2024-04-18 22:51:14 +0600

    Make useAuth custom hook for using useContext which is wrapping the AuthContext for managing state.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,22 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 // Make AuthContext using Context API
 const AuthContext = createContext();
 
 /**
+ * Make useAuth custom hook
+ * @returns
+ */
+export function useAuth() {
+	return useContext(AuthContext);
+}
+
+/**
  * AuthProvider is the authentication provider
  * It will wrap the enter application
+ * We use loading and currentUser local states for handling
+ * the application components and get the current user from
+ * the firebase while login/signup
  * @param {*} param0
  * @returns
  */
