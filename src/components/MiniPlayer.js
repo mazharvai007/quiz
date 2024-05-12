0558f45ffe77aee48e1088095face7417cdb888d
mazhar <swopnil420@gmail.com>
2024-05-12 12:14:54 +0600

    miniplayer is working now by clicking on the corresponding button on the Quiz page

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -1,14 +1,35 @@
+import { useRef, useState } from 'react';
 import CourseImg from '../assets/images/reactcourse.png';
 import classes from '../styles/MiniPlayer.module.css';
 
 function MiniPlayer() {
+	const buttonRef = useRef();
+	const [status, setStatus] = useState(false);
+
+	function toggleMiniPlayer() {
+		if (!status) {
+			buttonRef.current.classList.remove(classes.floatingBtn);
+			setStatus(true);
+		} else {
+			buttonRef.current.classList.add(classes.floatingBtn);
+			setStatus(false);
+		}
+	}
+
 	return (
 		<>
-			<div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+			<div
+				className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+				ref={buttonRef}
+				onClick={toggleMiniPlayer}
+			>
 				<span className={`material-icons-outlined ${classes.open}`}>
 					play_circle_filled
 				</span>
-				<span className={`material-icons-outlined ${classes.close}`}>
+				<span
+					className={`material-icons-outlined ${classes.close}`}
+					onClick={toggleMiniPlayer}
+				>
 					close
 				</span>
 				<img src={CourseImg} alt="Course" />
